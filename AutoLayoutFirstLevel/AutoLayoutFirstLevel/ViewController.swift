53d492c6cd41d2c12bd04cf5f1a0fccd7503443c
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-19 19:37:51 -0300

    feat: introduce base project to auto layout first level

---
+++ AutoLayoutFirstLevel/AutoLayoutFirstLevel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutFirstLevel
+//
+//  Created by Rhian Lopes da Costa on 19/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
