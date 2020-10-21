7fb604d09cf23f739fec9174669fca7c7fe8af7b
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-21 20:59:29 -0300

    feat: introduce base project

--- AutoLayoutFirstLevel/AutoLayoutFirstLevel/ViewController.swift
+++ AutoLayoutFirstLevel/AutoLayoutFirstLevel/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var botaoAdicionarFavorito: UIButton!
+    
+    @IBOutlet weak var selecaoQuantidade: UIView!
+    
+    @IBOutlet weak var selecaoTamanho: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.botaoAdicionarFavorito.layer.borderWidth = 1
+        self.botaoAdicionarFavorito.layer.borderColor = UIColor(named: "CinzaClaro")?.cgColor
+        
+        self.selecaoQuantidade.layer.borderWidth = 1
+        self.selecaoQuantidade.layer.borderColor = UIColor(named: "CinzaClaro")?.cgColor
+        
+        self.selecaoTamanho.layer.borderWidth = 1
+        self.selecaoTamanho.layer.borderColor = UIColor(named: "CinzaClaro")?.cgColor
     }
 
 }
