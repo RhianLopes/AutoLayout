922037dc3e64efac0e56b1e31935976d4e69d99f
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-21 21:41:27 -0300

    feat: introduce top buttons

--- AutoLayoutSecondLevel/AutoLayoutSecondLevel/ViewController.swift
+++ AutoLayoutSecondLevel/AutoLayoutSecondLevel/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var botaoFechar: UIButton!
+    @IBOutlet weak var botaoCarrinho: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.botaoFechar.layer.cornerRadius = botaoFechar.frame.width / 2
+        self.botaoCarrinho.layer.cornerRadius = botaoCarrinho.frame.width / 2
     }
 
 
