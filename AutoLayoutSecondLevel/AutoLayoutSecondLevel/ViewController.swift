3113f10bfbc7767a193fce99132e624349b6c171
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-21 21:21:32 -0300

    feat: base second level project

---
+++ AutoLayoutSecondLevel/AutoLayoutSecondLevel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutSecondLevel
+//
+//  Created by Rhian Lopes da Costa on 21/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
