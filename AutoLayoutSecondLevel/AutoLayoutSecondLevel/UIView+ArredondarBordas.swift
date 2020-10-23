a2c1209b94be97d032c244ae97e2cfb730930e00
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-22 22:00:46 -0300

    feat: introduce initial implementation second level auto layout

---
+++ AutoLayoutSecondLevel/AutoLayoutSecondLevel/UIView+ArredondarBordas.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+ArredondarBordas.swift
+//  AutoLayoutSecondLevel
+//
+//  Created by Rhian Lopes da Costa on 22/10/20.
+//
+
+import UIKit
+
+extension UIView {
+   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
--- AutoLayoutSecondLevel/AutoLayoutSecondLevel/ViewController.swift
+++ AutoLayoutSecondLevel/AutoLayoutSecondLevel/ViewController.swift
@@ -12,13 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var botaoFechar: UIButton!
     @IBOutlet weak var botaoCarrinho: UIButton!
     
+    @IBOutlet weak var menuApresentacaoView: UIView!
+    @IBOutlet weak var avaliacaoView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.botaoFechar.layer.cornerRadius = botaoFechar.frame.width / 2
         self.botaoCarrinho.layer.cornerRadius = botaoCarrinho.frame.width / 2
+        menuApresentacaoView.roundCorners(corners: [.topLeft, .topRight], radius: 50.0)
+        avaliacaoView.layer.cornerRadius = avaliacaoView.frame.height / 2
     }
 
 
 }
+
+
 
