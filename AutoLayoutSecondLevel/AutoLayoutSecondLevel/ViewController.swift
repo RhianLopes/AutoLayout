b1934897a9da97343e3221d8a8d5974356265b53
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-23 21:10:45 -0300

    refac: refac the base project

--- AutoLayoutSecondLevel/AutoLayoutSecondLevel/ViewController.swift
+++ AutoLayoutSecondLevel/AutoLayoutSecondLevel/ViewController.swift
@@ -11,17 +11,16 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var botaoFechar: UIButton!
     @IBOutlet weak var botaoCarrinho: UIButton!
-    
     @IBOutlet weak var menuApresentacaoView: UIView!
-    @IBOutlet weak var avaliacaoView: UIView!
+    @IBOutlet weak var avaliacaoView: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.botaoFechar.layer.cornerRadius = botaoFechar.frame.width / 2
         self.botaoCarrinho.layer.cornerRadius = botaoCarrinho.frame.width / 2
-        menuApresentacaoView.roundCorners(corners: [.topLeft, .topRight], radius: 50.0)
-        avaliacaoView.layer.cornerRadius = avaliacaoView.frame.height / 2
+        self.menuApresentacaoView.roundCorners(corners: [.topLeft, .topRight], radius: 50.0)
+        self.avaliacaoView.layer.cornerRadius = avaliacaoView.frame.height / 2
     }
 
 
