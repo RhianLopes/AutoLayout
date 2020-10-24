152f8a14fd80568f7e29943a14b9eadb3b8f6fbd
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-10-24 08:27:06 -0300

    feat: introduce base project third level

---
+++ AutoLayoutThirdLevel/AutoLayoutThirdLevel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutThirdLevel
+//
+//  Created by Rhian Lopes da Costa on 24/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
